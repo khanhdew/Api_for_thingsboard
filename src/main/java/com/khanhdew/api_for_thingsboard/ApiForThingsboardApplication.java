6b924432ee269d9255442ddeb7f38582049d312a
khanhdew <khanhdeptrai292004@gmail.com>
2024-08-14 10:22:59 +0700

    Initial commit

---
+++ src/main/java/com/khanhdew/api_for_thingsboard/ApiForThingsboardApplication.java
@@ -0,0 +1,13 @@
+package com.khanhdew.api_for_thingsboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForThingsboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiForThingsboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khanhdew/api_for_thingsboard/ApiForThingsboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanhdew.api_for_thingsboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForThingsboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
